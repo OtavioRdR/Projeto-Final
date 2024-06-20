fb59f4faf5fc2ca21c5aef7bac27252ad745c1c7
OtavioRdR <otaviorosa.rdr@gmail.com>
2024-06-19 22:40:06 -0300

    first commit

---
+++ src/test/java/com/apifutebol/apifutebol/ApifutebolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apifutebol.apifutebol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApifutebolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
